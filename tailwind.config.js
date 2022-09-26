d9bc84581eba28677e626671531e2304bdaf02eb
taoufiq lotfi <leh.lotfi@gmail.com>
2022-09-26 22:22:55 +0100

    added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      main: {
+        50: '#f2f5fb',
+        100: '#e6edf9',
+        200: '#d2dcf3',
+        300: '#b7c4ea',
+        400: '#9aa5df',
+        500: '#8189d3',
+        600: '#6769c4',
+        700: '#5656ac',
+        800: '#48498b',
+        900: '#1e1f35',
+      },
+    },
   },
   plugins: [],
 };

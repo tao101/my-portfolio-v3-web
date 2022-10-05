e61d6d293240d8444f6a10daa847b3ed1b19d2b5
taoufiq lotfi <leh.lotfi@gmail.com>
2022-10-05 13:06:46 +0100

    changed using a to next link

--- components/home/index.js
+++ components/home/index.js
@@ -2,6 +2,7 @@ import SocialIcons from '../socialIcons';
 import { motion, AnimatePresence } from 'framer-motion';
 import { TypeAnimation } from 'react-type-animation';
 import { urlFor } from '../../lib/sanity';
+import Link from 'next/link';
 
 export default function Home({ settings, homePage }) {
   const description = homePage?.description;
@@ -135,29 +136,28 @@ export default function Home({ settings, homePage }) {
           }}
           className=" z-20 absolute left-0 top-auto bottom-10 w-full justify-between text-center"
         >
-          <a
-            href="/#about"
-            className="cursor-pointer z-20 text-[#bfbecb] text-xs font-medium uppercase tracking-widest transition-all hover:text-secondary-400 antialiased "
-          >
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 24 24"
-              className="inline animate-bounce text-base hover:text-secondary-400"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g>
-                <path fill="none" d="M0 0h24v24H0z"></path>
-                <path d="M13 16.172l5.364-5.364 1.414 1.414L12 20l-7.778-7.778 1.414-1.414L11 16.172V4h2v12.172z"></path>
-              </g>
-            </svg>
-            <span className="pl-2 text-xs antialiased z-20 hover:text-secondary-400 text-[#bfbecb]">
-              SCROLL DOWN
-            </span>
-          </a>
+          <Link href="/#about">
+            <a className="cursor-pointer z-20 text-[#bfbecb] text-xs font-medium uppercase tracking-widest transition-all hover:text-secondary-400 antialiased ">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 24 24"
+                className="inline animate-bounce text-base hover:text-secondary-400"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g>
+                  <path fill="none" d="M0 0h24v24H0z"></path>
+                  <path d="M13 16.172l5.364-5.364 1.414 1.414L12 20l-7.778-7.778 1.414-1.414L11 16.172V4h2v12.172z"></path>
+                </g>
+              </svg>
+              <span className="pl-2 text-xs antialiased z-20 hover:text-secondary-400 text-[#bfbecb]">
+                SCROLL DOWN
+              </span>
+            </a>
+          </Link>
         </motion.div>
       </section>
     </>

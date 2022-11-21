39797b599941f9edbc12aefe7ae8105ebb45a8c7
taoufiq lotfi <leh.lotfi@gmail.com>
2022-11-21 21:36:20 +0100

    added reviews

--- components/reviews/index.js
+++ components/reviews/index.js
@@ -1,13 +1,105 @@
 import SectionTitle from '../sectionTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import ReviewItem from './reviewItem';
+import { Navigation, Pagination } from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { useSwiper } from 'swiper/react';
+import { useRef } from 'react';
 
 export default function Reviews({ homePage }) {
+  let reviews = homePage?.reviews;
+  const swiperRef = useRef(null);
+
   return (
     <section
       id="reviews"
-      className="mb-3  mt-24 z-20 flex flex-col   justify-center items-center "
+      className="mb-3  mt-24 z-20 flex flex-col overflow-visible	  justify-center items-center "
     >
       <SectionTitle title="CLIENT REVIEWS" subTitle="REVIEWS" />
-      <div className="w-full flex flex-col  lg:flex-row justify-between items-stretch  gap-7 pt-2 lg:pt-14"></div>
+      <div className="relative mb-12 xl:mb-0 w-full flex flex-co  lg:flex-row justify-between items-stretch overflow-visible	  gap-7 pt-2 lg:pt-4">
+        <button
+          className="bg-[#bfbecb] hover:scale-125 hover:bg-secondary-400 w-11 h-11 absolute right-[35%] xl:-right-16 -bottom-12 z-50 xl:top-[calc(50%_-_16px)] rounded-full flex justify-center items-center   "
+          onClick={() => {
+            swiperRef.current?.slideNext();
+          }}
+        >
+          <div className="bg-[#1e1f35]  text-[#bfbecb]  hover:text-secondary-400 rounded-full w-10 h-10 flex justify-center items-center  ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8  ml-0.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </div>
+        </button>
+        <button
+          className="bg-[#bfbecb] hover:scale-125 hover:bg-secondary-400 w-11 h-11 absolute left-[35%] xl:-left-16 -bottom-12 z-50 xl:top-[calc(50%_-_16px)] rounded-full flex justify-center items-center  "
+          onClick={() => {
+            swiperRef.current?.slidePrev();
+          }}
+        >
+          <div className="bg-[#1e1f35] text-[#bfbecb]  hover:text-secondary-400 rounded-full w-10 h-10 flex justify-center items-center  ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8  mr-0.5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </div>
+        </button>
+        <Swiper
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          loop={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Navigation, Pagination]}
+          spaceBetween={50}
+          slidesPerView={1}
+          id="always-be-swipin"
+          className=""
+          pagination={{ clickable: true }}
+        >
+          {reviews?.map((item) => {
+            return (
+              <SwiperSlide className="" key={item?._key}>
+                <ReviewItem review={item} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </section>
   );
 }
--- components/reviews/reviewItem.js
+++ components/reviews/reviewItem.js
@@ -2,7 +2,23 @@ import { urlFor } from '../../lib/sanity';
 import { motion, AnimatePresence } from 'framer-motion';
 import Link from 'next/link';
 import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
-export default function ReviewItem({}) {
-  return <div>sds</div>;
+export default function ReviewItem({ review }) {
+  return (
+    <div className="relative h-full group card hovercard circleprogress w-full  flex flex-col  items-stretch p-4 md:p-5">
+      <div className="absolute -top-10 z-50 left-8 p-1 w-16 h-16 bg-secondary-400 rounded-full 	">
+        <img
+          className="rounded-full	"
+          src={urlFor(review?.picture?.image)}
+          alt={review?.picture?.alt}
+        />
+      </div>
+      <h5 className="text-white mt-6 ">{review?.title}</h5>
+      <p className="mb-2 text-[#bfbecb]">{review?.subtitle}</p>
+      <div className="mb-3"></div>
+      <p className="text-[#bfbecb]">{review?.description}</p>
+    </div>
+  );
 }

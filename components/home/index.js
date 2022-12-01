8b5acb156276bec3393426ce705e532e10b241fd
taoufiq lotfi <leh.lotfi@gmail.com>
2022-12-01 13:22:02 +0100

    fix for scrolldown

--- components/home/index.js
+++ components/home/index.js
@@ -127,18 +127,16 @@ export default function Home({ settings, homePage }) {
             duration: 0.5,
             delay: 0.6,
           }}
-          className=" z-20 absolute left-0 top-auto bottom-10 w-full justify-between text-center"
+          className=" z-20 absolute left-0 top-auto bottom-5 w-full justify-between text-center"
         >
           <Link href="/#about">
-            <a className="cursor-pointer z-20 text-[#bfbecb] text-xs font-medium uppercase tracking-widest transition-all hover:text-secondary-400 antialiased ">
+            <a className="cursor-pointer  w-12 h-12 z-20 text-[#bfbecb] text-xs font-medium uppercase tracking-widest transition-all hover:text-secondary-400 antialiased ">
               <svg
                 stroke="currentColor"
                 fill="currentColor"
                 strokeWidth="0"
                 viewBox="0 0 24 24"
-                className="inline animate-bounce text-base hover:text-secondary-400"
-                height="1em"
-                width="1em"
+                className="w-8 h-8 sm:w-12 sm:h-12  inline animate-bounce text-base hover:text-secondary-400"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <g>
@@ -146,7 +144,7 @@ export default function Home({ settings, homePage }) {
                   <path d="M13 16.172l5.364-5.364 1.414 1.414L12 20l-7.778-7.778 1.414-1.414L11 16.172V4h2v12.172z"></path>
                 </g>
               </svg>
-              <span className="pl-2 text-xs antialiased z-20 hover:text-secondary-400 text-[#bfbecb]">
+              <span className="pl-2 sm:text-lg  antialiased z-20 hover:text-secondary-400 text-[#bfbecb]">
                 SCROLL DOWN
               </span>
             </a>
